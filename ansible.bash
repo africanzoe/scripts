b52409e3c992daba5669929b69946db7b6045da0
africanzoe <chafik.bel@gmail.com>
2017-11-13 23:12:56 +0100

    fix default inventory path

--- ansible.bash
+++ ansible.bash
@@ -5,26 +5,25 @@ _ansible_playbook ()
 
     COMPREPLY=()
     words=("${COMP_WORDS[@]}")
-    check=$(echo "${COMP_WORDS[@]}" | grep -- '-i ')
+    # Check if the option "-i or "--inventory-file" is present in the COMP_WORDS
+    check=$(echo "${COMP_WORDS[@]}" | egrep -- '-i |--inventory-file')
     if [[ "$check" ]]
     then
         HOSTS=$(echo "$check" | awk '/-i / || /--inventory-file / {print $(NF-1)}')
     else
-        HOSTS="/data/workshop/iac/ansible/hosts"
+        HOSTS="/etc/ansible/hosts"
     fi
 
     grps=$(awk '/^\[/ {print substr($1,2,length($1)-2)}' "${HOSTS}" | cut -d':' -f1 | sort -u)
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-
     case $prev in
         -l|--limit)
             COMPREPLY=( $(compgen -W "$grps" -- ${cur}) )
-            #COMPREPLY=( $( compgen -W 'one two three' -- "$cur" ) )
-                ;;
+        ;;
     esac
-  
+
       return 0
 } &&
 complete -F _ansible_playbook -o filenames ansible-playbook ansible-playbook.py
