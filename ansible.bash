8513deb1fc45becdf4d0fbc929b5b7b0c1b31c4f
africanzoe <chafik.bel@gmail.com>
2017-11-13 23:05:19 +0100

    customize the lookup according to the inventory file

--- ansible.bash
+++ ansible.bash
@@ -4,21 +4,27 @@ _ansible_playbook ()
     local cur prev grps words=() #split=false
 
     COMPREPLY=()
-        HOSTS="/etc/ansible/hosts"
-    grps=$(awk '/^\[/ {print substr($1,2,length($1)-2)}' $HOSTS | cut -d':' -f1 | sort -u)
-        words=("${COMP_WORDS[@]}")
+    words=("${COMP_WORDS[@]}")
+    check=$(echo "${COMP_WORDS[@]}" | grep -- '-i ')
+    if [[ "$check" ]]
+    then
+        HOSTS=$(echo "$check" | awk '/-i / || /--inventory-file / {print $(NF-1)}')
+    else
+        HOSTS="/data/workshop/iac/ansible/hosts"
+    fi
+
+    grps=$(awk '/^\[/ {print substr($1,2,length($1)-2)}' "${HOSTS}" | cut -d':' -f1 | sort -u)
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  case $prev in
-      -l|--limit)
-          COMPREPLY=( $(compgen -W "$grps" -- ${cur}) )
-          #COMPREPLY=( $( compgen -W 'one two three' -- "$cur" ) )
-              ;;
-  esac
 
-    return 0
+    case $prev in
+        -l|--limit)
+            COMPREPLY=( $(compgen -W "$grps" -- ${cur}) )
+            #COMPREPLY=( $( compgen -W 'one two three' -- "$cur" ) )
+                ;;
+    esac
+  
+      return 0
 } &&
 complete -F _ansible_playbook -o filenames ansible-playbook ansible-playbook.py
-
-# TODO: get the inventory dynamically
