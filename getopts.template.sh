e5700fec1cbcc47317fa9bf52cb5a66c043dcb27
cbelhaoues <cbelhaoues@online.com>
2018-01-16 17:23:14 +0100

    Add bash colors

--- getopts.template.sh
+++ getopts.template.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+# Bash colors
+# ex: printf "$green this is green message $reset\n"
+reset="\033[0m"
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+cyan="\033[36m"
+white="\033[37m"
+
 while getopts :a: opt; do
   case $opt in
     a)
